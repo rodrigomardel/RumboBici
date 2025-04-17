707d89706e37a06bc765d984697c0b48b150e163
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-04-17 18:27:42 +0200

    [FEAT] data sql y update doc

--- db/data/data-ruta.sql
+++ db/data/data-ruta.sql
@@ -1,3 +1,8 @@
 -- Insertar datos en la tabla ruta
 INSERT INTO ruta (nombre_ruta, localidad_ruta, kilometros_ruta, fecha_ruta, id_categoria) VALUES
-('BTT Castillo', 'Burgos', '25', '2025-04-10', '1');
+('BTT Castillo', 'Burgos', '25', '2025-04-10', '1'),
+('La Pinilla', 'Segovia', '30', '2022-03-12', '2'),
+('Via Verde Ojos Negros', 'Teruel', '160', '2024-04-17', '3'),
+('Burgos - Lagunas de Neila', 'Burgos', '180', '2020-04-10', '4'),
+('Camino del Norte', 'Asturias', '150', '2018-02-10', '5'),
+('Barcelona Paseo Marítimo', 'Barcelona', '20', '2025-04-10', '6');
--- db/data/data-usuario_ruta.sql
+++ db/data/data-usuario_ruta.sql
@@ -1,3 +1,12 @@
 -- Insertar datos en la tabla usuario_ruta
 INSERT INTO usuario_ruta (id_usuario, id_ruta, fecha_realizacion) VALUES
-(1, 1, '2025-04-10');
+(1, 1, '2025-04-10'),  -- user1 hizo BTT Castillo
+(1, 2, '2025-04-12'),  -- user1 hizo La Pinilla
+(2, 2, '2023-05-15'),  -- user2 hizo La Pinilla
+(2, 3, '2024-04-20'),  -- user2 hizo Via Verde
+(3, 1, '2025-04-11'),  -- user3 hizo BTT Castillo
+(3, 4, '2021-07-10'),  -- user3 hizo Burgos - Lagunas
+(4, 5, '2019-02-15'),  -- user4 hizo Camino del Norte
+(7, 6, '2025-04-10'),  -- user5 hizo Barcelona Marítimo
+(8, 1, '2025-04-11'),  -- user6 hizo BTT Castillo
+(8, 3, '2024-04-17');  -- user6 hizo Via Verde
