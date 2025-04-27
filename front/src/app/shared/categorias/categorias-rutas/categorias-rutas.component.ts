cda7dc6fcd2c28b9e76dce6645adeffd64408060
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-04-27 10:00:34 +0200

    [FEAT] Frontend: color rutas por categoría

--- front/src/app/shared/categorias/categorias-rutas/categorias-rutas.component.ts
+++ front/src/app/shared/categorias/categorias-rutas/categorias-rutas.component.ts
@@ -18,6 +18,16 @@ export class CategoriasRutasComponent implements OnInit {
   relacionRutasUsuario: UsuarioRuta[] = [];
   /** ID del usuario de la sesión correspondiente transformado a Number para obtener sus rutas de la tabla usuario_ruta  */
   idSessionUser: number | null = localStorage.getItem('idUsuario') !== null ? Number(localStorage.getItem('idUsuario')) : null;
+   /** Color del contenedor de la ruta en función de su categoría */
+   coloresCategoria: { [key: number]: string } = {
+    1: 'rgba(45, 250, 26, 0.23)',
+    2: 'rgba(255, 49, 193, 0.20)',
+    3: 'rgba(255, 217, 0, 0.25)',
+    4: 'rgba(255, 115, 0, 0.36)',
+    5: 'rgba(27, 198, 255, 0.30)',
+    6: 'rgba(15, 0, 218, 0.18)',
+  };
+
 
   constructor(private route: ActivatedRoute, private usuarioRutaService: UsuarioRutaService) { }
 
@@ -41,5 +51,14 @@ export class CategoriasRutasComponent implements OnInit {
         console.error('Error de sesión');
       }
     });
+  }
+
+   /**
+   * Asigna a cada ruta el color correspondiente a su categoría
+   * @param idCategoria ID de la categoría correspondiente
+   * @returns color correspondiente o blanco en caso contrario
+   */
+   obtenerColorFondo(idCategoria: number): string {
+    return this.coloresCategoria[idCategoria] || '#ffffff';
   }
 }
